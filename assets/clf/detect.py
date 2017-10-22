9e91a9d630de23b5df285d0d9d218eff69419157
pratyushpal <pratyush98pal@gmail.com>
2017-10-22 09:08:53 -0400

    WIP

--- assets/clf/detect.py
+++ assets/clf/detect.py
@@ -237,6 +237,23 @@ while True:
                                    args=(args['alarm'], ))
                         t.deamon = True
                         t.start()
+                    cap = cv2.VideoCapture(0)
+                    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+                    out = cv2.VideoWriter('output.avi',fourcc,3.0,(640,480))
+
+                    while(cap.isOpened()):
+                        ret, frame = cap.read()
+                        if ret == True:
+                            frame=cv2.flip(frame,0)
+                            out.write(frame)
+                            if cv2.waitKey(1) & 0xFF == ord('q'):
+                                break
+                        else:
+                            break
+
+                    cap.release()
+                    out.release()
+                    cv2.destroyAllWindows()
 
                     # draw an alarm on the frame
 
