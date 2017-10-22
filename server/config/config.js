c008ccabce313ce0306ad6db34b19afc8070c4d6
justinborromeo <jborrome@edu.uwaterloo.ca>
2017-10-22 03:31:49 -0400

    removed unnecessary variable

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,6 @@
 var config = {};
 config.port=443;
 config.mongodbconnectionstring = 'mongodb://localhost/DrowsyDriving';
-config.domainname = "http://localhost:443";
 config.hmacsecret = "qoweutp89weg7wp32io4nwehgipouaewyt";
 
 module.exports = config;
